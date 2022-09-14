a103a3242451aa1c8894abc1066951bcaa9d0d1d
RiteshSingh9 <raunak.singh.visen.9@gmail.com>
2022-09-14 09:29:56 +0530

    using props in styled  components

--- src/components/Buttons/Button.styles.js
+++ src/components/Buttons/Button.styles.js
@@ -2,13 +2,13 @@ import styled from "styled-components";
 
 export const StyledButton = styled.button`
 	border: 1px solid #4caf50;
-	background-color: #1FAA59;
-	color: #eee;
-	padding: 15px 32px;
-	text-align: center;
-	text-decoration: none;
-	display: inline-block;
-	font-size: 16px;
-	cursor: pointer;
-	transition: all 0.5s ease-out;
+	background-color: ${(props) => props.variant === 'outline' ? '#eee' : '#1FAA59'};
+	color:  ${(props) => props.variant === 'outline' ? '#1FAA59' : '#eee'};
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline - block;
+    font-size: 16px;
+    cursor: pointer;
+    transition: all 0.5s ease - out;
 `;
