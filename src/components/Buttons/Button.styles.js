b317e86e68bf2edb5e5523dcfb7190b576ca245a
RiteshSingh9 <raunak.singh.visen.9@gmail.com>
2022-09-14 09:25:56 +0530

    basic styled components

---
+++ src/components/Buttons/Button.styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+	border: 1px solid #4caf50;
+	background-color: #1FAA59;
+	color: #eee;
+	padding: 15px 32px;
+	text-align: center;
+	text-decoration: none;
+	display: inline-block;
+	font-size: 16px;
+	cursor: pointer;
+	transition: all 0.5s ease-out;
+`;
